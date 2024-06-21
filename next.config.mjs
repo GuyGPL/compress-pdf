355547f4f0df3d757305b01993ebcb29c6bd6c11
GuyGPL <poramutinb@hotmail.com>
2024-06-21 14:59:41 +0700

    add config and dependency file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+            config.resolve.alias['canvas'] = false;
+
+        return config;
+    },
+    future: {
+        webpack5: true,
+    },
+};
 
 export default nextConfig;
